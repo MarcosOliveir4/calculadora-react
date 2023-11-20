015eb09a7e0d8d96a76bdcf81f78d10fcadf0e7f
MarcosOliveir4 <marcosassis071@gmail.com>
2023-11-20 14:08:37 -0300

    feat: base do projeto com vite e vitest

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import GlobalCss from './styles/global.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GlobalCss />
+    <App />
+  </React.StrictMode>
+);
